8220184d329fee1547beefc4ff69b7b0f55284b2
fy-deng <46555482+fy-deng@users.noreply.github.com>
2023-04-16 16:31:23 +0800

    Update installReality.sh

--- installReality.sh
+++ installReality.sh
@@ -30,7 +30,7 @@ go install -v -trimpath -ldflags "-s -w -buildid=" -tags with_gvisor,with_quic,w
 
 sudo cp $(go env GOPATH)/bin/sing-box /usr/local/bin/
 sudo mkdir -p /var/lib/sing-box
-sudo mkdir -p /usr/local/etc/sing-box
+sudo mkdir -p /etc/sing-box
 
 pwd=`pwd`
 echo "\033[31m $pwd \033[0m" 
@@ -40,8 +40,8 @@ sudo systemctl daemon-reload
 
 
 echo "拉取初始singbox配置文件"
-curl -o config.json https://raw.githubusercontent.com/BoxXt/installReality/main/config.json && mv config.json /usr/local/etc/sing-box/config.json
-curl -o meta.yaml https://raw.githubusercontent.com/BoxXt/installReality/main/meta.yaml && mv meta.yaml /usr/local/etc/sing-box/meta.yaml
+curl -o config.json https://raw.githubusercontent.com/BoxXt/installReality/main/config.json && mv config.json /etc/sing-box/config.json
+curl -o meta.yaml https://raw.githubusercontent.com/BoxXt/installReality/main/meta.yaml && mv meta.yaml /etc/sing-box/meta.yaml
 
 
 echo "启动singbox用于安装reality"
@@ -54,19 +54,19 @@ echo -n "\033[32m 请输入你刚刚保存的密钥对中的私钥（PrivateKey�
 read pkey
 echo -n "\033[32m 请输入你刚刚保存的密钥对中的公钥（Pubilckey）: \033[0m"
 read pukey
-sed -in "s/pkey/$pkey/g" /usr/local/etc/sing-box/config.json
+sed -in "s/pkey/$pkey/g" /etc/sing-box/config.json
 
 echo "生成uuid"
 uuid=`sing-box generate uuid`
 echo $uuid
-sed -in "s/puuid/$uuid/g" /usr/local/etc/sing-box/config.json
+sed -in "s/puuid/$uuid/g" /etc/sing-box/config.json
 
 echo "安装openssl"
 sudo apt-get update && sudo apt-get install openssl
 shortid=`openssl rand -hex 8`
 echo "随机生成short_id"
 echo $shortid
-sed -in "s/pshortid/$shortid/g" /usr/local/etc/sing-box/config.json
+sed -in "s/pshortid/$shortid/g" /etc/sing-box/config.json
 
 echo "完成配置，启动singbox"
 echo ""
@@ -90,13 +90,13 @@ echo "\033[31m PublicKey: $pukey \033[0m"
 echo "\033[31m short_id: $shortid \033[0m"
 echo ""
 ip=`curl https://api.my-ip.io/ip -s`
-sed -in "s/vpsip/$ip/g" /usr/local/etc/sing-box/meta.yaml
-sed -in "s/pukey/$pukey/g" /usr/local/etc/sing-box/meta.yaml
-sed -in "s/pshortid/$shortid/g" /usr/local/etc/sing-box/meta.yaml
-sed -in "s/puuid/$uuid/g" /usr/local/etc/sing-box/meta.yaml
+sed -in "s/vpsip/$ip/g" /etc/sing-box/meta.yaml
+sed -in "s/pukey/$pukey/g" /etc/sing-box/meta.yaml
+sed -in "s/pshortid/$shortid/g" /etc/sing-box/meta.yaml
+sed -in "s/puuid/$uuid/g" /etc/sing-box/meta.yaml
 echo "\033[32m 以下是你的meta客户端所需要的可用示例配置文件： \033[0m"
 echo "========================================================="
-cat /usr/local/etc/sing-box/meta.yaml
+cat /etc/sing-box/meta.yaml
 echo "完成搭建"
 
 
